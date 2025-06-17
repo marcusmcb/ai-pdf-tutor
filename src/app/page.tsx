f7e899981abeda8fc763034ec47514367688878d
marcusmcb <marcusmcb@gmail.com>
2025-06-16 19:52:22 -0700

    fixed build dep errors, reinstalled tailwind and prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<main className='flex min-h-screen items-center justify-center'>
-			<h1 className='text-4xl font-bold text-blue-500'>
+			<h1 className='text-4xl font-bold text-red-500'>
 				Tailwind is working 🎉
 			</h1>
 		</main>
