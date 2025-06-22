6ca8a8f9851a7c6bdbba58d71aa1e377cb5b4243
marcusmcb <marcusmcb@gmail.com>
2025-06-22 13:09:06 -0700

    fixed type error on user property

--- pages/api/list-pdfs.ts
+++ pages/api/list-pdfs.ts
@@ -10,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const session = await getServerSession(req, res, authOptions as any);
+  const session = (await getServerSession(req, res, authOptions as any)) as any;
   if (!session || !session.user?.email) {
     return res.status(401).json({ error: "Unauthorized" });
   }
